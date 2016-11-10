fc124df74f3f37af8427dd1b384b21cda12b13bd
kaox <kaox@10.10.221.90>
2016-11-10 09:52:47 -0500

    Base

---
+++ app/config/parameters.php
@@ -0,0 +1,22 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => NULL,
+    'database_name' => 'prestashop',
+    'database_user' => 'root',
+    'database_password' => NULL,
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'ThisTokenIsNotSoSecretChangeIt',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => NULL,
+    'locale' => 'en-US',
+    'new_cookie_key' => 'def00000d13245fff4239e02c6803e79a97f49e47165a8564462dc2126600ed04c18fc00f9ff98aead689ffded3a5abc6af842b77aba8dd492f38912f23aa9ea74d35430',
+  ),
+);
